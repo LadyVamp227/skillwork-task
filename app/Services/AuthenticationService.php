b4a2f1005ad343b6ff93a8c647ab62e54ab9393e
LadyVirus <g.rudarska@abv.bg>
2021-08-25 18:44:10 +0300

    Add form request ArticleRequest

--- app/Services/AuthenticationService.php
+++ app/Services/AuthenticationService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,16 +15,16 @@ final class AuthenticationService
      * @param $data
      * @return array
      */
-    public function login($data) : array
+    public function login($data) : Collection
     {
         $user = User::where('email', $data['email'])->first();
 
         if (!$user || !Hash::check($data['password'], $user->password)) {
-            return ['response' => 'Invalid email or password', 'status' => 422];
+            return collect(['response' => 'Invalid email or password', 'status' => 422]);
         }
         $token = $user->createToken('authToken')->plainTextToken;
 
-        return ['response' => ['access_token' => $token, 'token_type' => 'Bearer', 'user' => $user], 'status' => 200];
+        return collect(['response' => ['access_token' => $token, 'token_type' => 'Bearer', 'user' => $user], 'status' => 200]);
     }
 
     /**
